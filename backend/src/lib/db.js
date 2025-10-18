8a4e1ba3a71a45f2aefc297b138655ae03808ea3
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-10-18 02:27:39 +0200

    Update env path

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
     try {
-        const { MONGO_URI } = process.env;
+        const { MONGO_URI } = ENV;
         if (!MONGO_URI) {
             throw new Error("MONGO_URI is not set");
         }
         
-        const connection = await mongoose.connect(process.env.MONGO_URI);
+        const connection = await mongoose.connect(ENV.MONGO_URI);
         console.log("MONGODB CONNECTED: ", connection.connection.host);
     } catch (error) {
         console.error("Error connection to MONGODB: ", error);
